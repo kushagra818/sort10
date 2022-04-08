0dc3940787629d9aa4e4411bc7a4bb54570b0423
e21bcau0031 <e21bcau0031@SCSET215BLA10.bennett.edu.in>
2022-04-08 13:01:46 +0530

    bubble sort

---
+++ src/sort2/bubblesort.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package sort2;
+
+/**
+ * @author e21bcau0031
+ *
+ */
+public class bubblesort {
+
+	/**
+	 * @param args
+	 */
+		// TODO Auto-generated method stub
+		/**
+		 * 
+		 */
+		
+			/**
+			 * @param args
+			 */
+			public static void main(String[] args) { 
+				// TODO Auto-generated method stub
+				int[] arr = new int [10];
+				  arr[0] = 10;
+				  arr[1] = 9;
+				  arr[2] = 8;
+				  arr[3] = 7;
+				  arr[4] = 6;
+				  arr[5] = 5;
+				  arr[6] = 4;
+				  arr[7] = 3;
+				  arr[8] = 2;
+				  arr[9] = 1;
+						    
+			        int temp = 0;  
+			         for(int i=0; i <10; i++){  
+			                 for(int j=1; j <10-i; j++){  
+			                          if(arr[j-1] > arr[j]){  
+			                                 //swap elements  
+			                                 temp = arr[j-1];  
+			                                 arr[j-1] = arr[j];  
+			                                 arr[j] = temp;  
+			                         }  
+			                          
+			                 }  
+			         } 
+			         
+			         for(int i=0; i<10; i++) {
+			        	 System.out.print(arr[i]);
+			         }
+			}
+
+
+			
+
+
+	}
+
+
